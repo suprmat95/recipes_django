6e6930fff4762e084cd43db8636a6914c5655661
Matteo D'Ospina <matteodospina@gmail.com>
2020-10-25 18:05:08 +0100

    now recipe page is setted

--- QuestionPlace/frontend/src/views/Question.vue
+++ QuestionPlace/frontend/src/views/Question.vue
@@ -9,15 +9,39 @@
                 <div>
                     <img id="picture"/>
                 </div>
+                <br/>
+                <hr/>
+                <h3 class="mb-3 align-left">Ingredienti</h3>
+                <div class="row">
+                    <div class="col-sm">
+                        <ul>
+                            <li v-for="(ingredient, index) in ingredients" :key="index">
+                                <b>{{ ingredient.name }}  {{ ingredient.quantity }} {{ingredient.unity}}</b>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <br/>
+                <hr/>
+                <h3 class="mb-3 align-left">Passaggi</h3>
+                <div class="row">
+                    <div class="col-sm">
+                        <ul>
+                            <li v-for="(passage, index) in passages" :key="'A'+index">
+                                <b >{{ passage.body }}</b>
+                                <img :src="passage.picture">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-                <QuestionActions
-                        v-if="isOwner"
-                        :slug="slug"/>
-                <p class="mb-0"> Domanda aggiunta da:
+                <QuestionActions v-if="isOwner" :slug="slug"/>
+                <p class="mb-0">
+                    Domanda aggiunta da:
                     <span class="author-name">{{ question.author }}</span>
                 </p>
                 <p>{{ question.created_at }}</p>
-                <hr>
+                <hr/>
                 <template v-if="userHasAnswered">
                     <p class="answer-added">Hai risposto a questa domanda.</p>
                 </template>
@@ -27,27 +51,27 @@
                             Aggiungi una risposta alla domanda
                         </div>
                         <div class="card-block">
-                        <textarea
-                                v-model="newAnswerBody"
-                                class="form-control"
-                                placeholder="Aggiungi una risposta alla domanda"
-                                rows="5"
-                        ></textarea>
+              <textarea
+                      v-model="newAnswerBody"
+                      class="form-control"
+                      placeholder="Aggiungi una risposta alla domanda"
+                      rows="5"
+              ></textarea>
                         </div>
                         <div class="card-footer px-3">
-                            <button type="submit" class="btn btn-sm btn-success">Aggiungi risposta</button>
+                            <button type="submit" class="btn btn-sm btn-success">
+                                Aggiungi risposta
+                            </button>
                         </div>
                     </form>
-                    <p class="error mt-2">{{error}}</p>
+                    <p class="error mt-2">{{ this.error }}</p>
                 </template>
                 <template v-else>
-                    <button
-                            class="btn btn-sm btn-success"
-                            @click="showForm=true"
-                    >Rispondi alla Domanda
+                    <button class="btn btn-sm btn-success" @click="showForm = true">
+                        Rispondi alla Domanda
                     </button>
                 </template>
-                <hr>
+                <hr/>
                 <AnswerComponent
                         v-for="(answer, index) in answers"
                         :answer="answer"
@@ -60,8 +84,9 @@
                     <button
                             v-show="next"
                             @click="getQuestionAnswers"
-                            class="btn btn-sm btn-outline-succes"> Carica Ancora
-
+                            class="btn btn-sm btn-outline-succes"
+                    >
+                        Carica Ancora
                     </button>
                 </div>
             </div>
@@ -100,7 +125,7 @@
                 error: null,
                 next: null,
                 requestUser: null
-            }
+            };
         },
         computed: {
             isOwner() {
@@ -115,69 +140,66 @@
                 document.title = title;
             },
             setRequestUser() {
-                this.requestUser = window.localStorage.getItem("username")
+                this.requestUser = window.localStorage.getItem("username");
             },
             getQuestionData() {
-                let endpoint = `/api/questions/${this.slug}/`
-                apiService(endpoint)
-                    .then(data => {
-                        this.question = data;
-                        console.log(this.question)
-                        document.getElementById("picture").setAttribute("src", this.question.picture);
+                let endpoint = `/api/questions/${this.slug}/`;
+                apiService(endpoint).then(data => {
+                    this.question = data;
+                    console.log(this.question.passage);
+                    this.ingredients = this.question.ingredient;
+                    this.passages = this.question.passage;
+                    document
+                        .getElementById("picture")
+                        .setAttribute("src", this.question.picture);
 
-                        this.userHasAnswered = data.user_has_answered;
-                        this.setPageTitle(data.content)
-                        apiService(endpoint+"passages/")
-                        .then(passages =>{
-                            this.passages= passages.results;
-                            console.log(this.passages);
-                        })
-                    })
+                    this.userHasAnswered = data.user_has_answered;
+                    this.setPageTitle(data.content);
+                });
             },
             getQuestionAnswers() {
-                let endpoint = `/api/questions/${this.slug}/answers/`
+                let endpoint = `/api/questions/${this.slug}/answers/`;
                 if (this.next) {
                     endpoint = this.next;
                 }
                 this.loadingAnswers = true;
-                apiService(endpoint)
-                    .then(data => {
-                        this.answers.push(...data.results);
-                        this.loadingAnswers = false;
-                        if (data.next) {
-                            this.next = data.next;
-                        } else {
-                            this.next = null;
-                        }
-                    })
-
+                apiService(endpoint).then(data => {
+                    this.answers.push(...data.results);
+                    this.loadingAnswers = false;
+                    if (data.next) {
+                        this.next = data.next;
+                    } else {
+                        this.next = null;
+                    }
+                });
             },
             onSubmit() {
                 if (this.newAnswerBody) {
                     let endpoint = `/api/questions/${this.slug}/answer/`;
-                    apiService(endpoint, "POST", {body: this.newAnswerBody})
-                        .then(data => {
+                    apiService(endpoint, "POST", {body: this.newAnswerBody}).then(
+                        data => {
                             this.answers.unshift(data);
-                        })
+                        }
+                    );
                     this.newAnswerBody = null;
                     this.showForm = false;
                     this.userHasAnswered = true;
                     if (this.error) {
-                        this.error = null
+                        this.error = null;
                     }
                 } else {
-                    this.error = "Il campo non puo essere vuoto."
+                    this.error = "Il campo non puo essere vuoto.";
                 }
             },
             async deleteAnswer(answer) {
-                let endpoint = `/api/answers/${answer.id}/`
+                let endpoint = `/api/answers/${answer.id}/`;
                 try {
                     await apiService(endpoint, "DELETE");
                     //  this.answers.splice(this.answers.indexOf(answer), 1);
                     this.$delete(this.answers, this.answers.indexOf(answer));
-                    this.userHasAnswered = false
-                } catch {
-                    console.log(error);
+                    this.userHasAnswered = false;
+                } catch (err) {
+                    console.log(err);
                 }
             }
         },
@@ -186,7 +208,7 @@
             this.getQuestionAnswers();
             this.setRequestUser();
         }
-    }
+    };
 </script>
 
 <style scoped>
